e24a493b63e1a73c734a0633890d56457e09855c
tolossamuel <tolossamuel1@gmail.com>
2025-12-11 16:07:32 +0300

    setup otp for phone sms

--- app/utils/sms_service.py
+++ app/utils/sms_service.py
@@ -6,7 +6,7 @@ def send_sms(phone_number: str, otp: str) -> dict:
     try:
         client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
         message = client.messages.create(
-            body=f"Your OTP code is: {otp}",
+            body=otp,
             from_=settings.TWILIO_PHONE_NUMBER,
             to=phone_number
         )
